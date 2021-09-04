ad73d1d6458a37e14ed5420b4a04359fc910e619
DMpablo <phdemarco@gmail.com>
2021-09-04 16:11:39 -0300

    posibles  mejoras SEO y css

--- src/componentes/newNote/newNote.jsx
+++ src/componentes/newNote/newNote.jsx
@@ -1,37 +1,33 @@
 import styles from "./newNotes.module.scss";
 
-export const NewNotes = ({newNote, editNote, createNote,cancelNewNote}) => {
+export const NewNotes = ({ newNote, editNote, createNote, cancelNewNote }) => {
+  return newNote.color !== undefined ? (
+    <div className={`${styles.containerCard}`}>
+      <div>
+        <textarea
+          rows="auto"
+          cols="auto"
+          placeholder="escribe una nota"
+          className={`${styles[newNote.color]} ${styles.card}`}
+          onChange={({ target }) => editNote(newNote.color, target.value)}
+        ></textarea>
 
-  return (
-     
-         newNote.color !== undefined ? ( 
-         <div className={`${styles.containerCard}`}>
-          
-            <div> 
-              <textarea
-                rows="auto"
-                cols="auto"
-                placeholder="escribe una nota"
-                className={`${styles[newNote.color]} ${styles.card}`}
-                onChange={({target}) => editNote( newNote.color, target.value )}
-              ></textarea>
-              
-              <button
-                className={`${styles[newNote.color]} ${styles.deleteNote}`}
-                onClick={() => cancelNewNote()}
-              >
-                Cancel
-              </button>
-              
-              <button
-              className={`${styles[newNote.color]} ${styles.createNote}`}
-              onClick={() => createNote(newNote)}
-              >
-              +
-              </button>
-            </div>
-          </div>)
-          :
-          ""
+        <button
+          className={`${styles[newNote.color]} ${styles.cancelNote}`}
+          onClick={() => cancelNewNote()}
+        >
+          Cancel
+        </button>
+
+        <button
+          className={`${styles[newNote.color]} ${styles.createNote}`}
+          onClick={() => createNote(newNote)}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  ) : (
+    ""
   );
 };
